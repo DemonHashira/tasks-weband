82b373d2b64d6f0312f25a428f66352f88828f2d
DemonHashira <v.logodazhki@outlook.com>
2024-06-12 16:56:46 +0300

    Half done second task

--- likes-commentaries/js/script.js
+++ likes-commentaries/js/script.js
@@ -1,24 +1,17 @@
 // Immediately Invoked Function Expression (IIFE) to avoid polluting the global namespace
 (function () {
   window.onload = function () {
-    hideAllFields();
-    handleTypeChange();
+    initializeLikeButton();
   };
 })();
 
-document.querySelector(".like-icon").addEventListener("click", function () {
-  this.classList.toggle("liked");
-});
-
-document
-  .getElementById("comment-button")
-  .addEventListener("click", function () {
-    var commentInput = document.getElementById("comment-input");
-    var commentText = commentInput.value.trim();
-    if (commentText) {
-      var commentElement = document.createElement("p");
-      commentElement.textContent = commentText;
-      document.getElementById("comments").appendChild(commentElement);
-      commentInput.value = "";
-    }
+// Function to toggle the state when the like button is clicked
+function initializeLikeButton() {
+  var likeButton = document.querySelector(".like-button");
+  likeButton.addEventListener("click", function () {
+    var icon = this.querySelector("i");
+    icon.classList.toggle("far");
+    icon.classList.toggle("fas");
+    icon.classList.toggle("liked");
   });
+}
