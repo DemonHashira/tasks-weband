1d20c882ec8cfa2eded3640423f731aa9770fbfc
DemonHashira <v.logodazhki@outlook.com>
2024-06-13 12:04:30 +0300

    Reworked code to follow best practices for task 1

--- dynamic-fields/js/script.js
+++ dynamic-fields/js/script.js
@@ -1,37 +1,34 @@
 // Immediately Invoked Function Expression (IIFE) to avoid polluting the global namespace
-(function () {
-  window.onload = function () {
+(() => {
+  window.onload = () => {
     hideAllFields();
     handleTypeChange();
   };
 })();
 
 // Function to hide all fields initially
-function hideAllFields() {
+const hideAllFields = () => {
   document.getElementById("invoiceFields").style.display = "none";
   document.getElementById("messageFields").style.display = "none";
   document.getElementById("requestFields").style.display = "none";
   document.getElementById("extractFields").style.display = "none";
-}
+};
 
-function handleTypeChange() {
+const handleTypeChange = () => {
   // Get the selected element
-  var typeSelect = document.getElementById("type");
+  const typeSelect = document.getElementById("type");
 
   // Add event listener for the 'change' event
   typeSelect.addEventListener("change", function () {
     // Hide all the additional fields by default
     hideAllFields();
 
-    // Check the value of the selected option
-    if (this.value === "invoice") {
-      document.getElementById("invoiceFields").style.display = "block";
-    } else if (this.value === "message") {
-      document.getElementById("messageFields").style.display = "block";
-    } else if (this.value === "request") {
-      document.getElementById("requestFields").style.display = "block";
-    } else if (this.value === "extract") {
-      document.getElementById("extractFields").style.display = "block";
+    // Get the element corresponding to the selected option
+    const selectedField = document.getElementById(this.value + "Fields");
+
+    // Display the selected field
+    if (selectedField) {
+      selectedField.style.display = "block";
     }
   });
-}
+};
